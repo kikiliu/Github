dcfbfc6093f254e775094c21798d7ba268b005a2
kikiliu <kikiliu@berkeley.edu>
2013-11-13 20:45:17 -0800

    change folder

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {2, 4, -1, -5, 8, 10, -11};
+            Max_Sum(numbers);
+        }
+        static int Max_Sum(int[] numbers)
+        {
+                        
+            int max_sum = 0;
+            int temp_sum = 0;
+            foreach (int element in numbers)
+            {
+                temp_sum += element;
+                if (element > 0)
+                {
+                    max_sum = Math.Max(max_sum, temp_sum);
+                }
+                else if (element <= 0 && temp_sum <= 0)
+                {
+                    temp_sum = 0;
+                }
+             }
+            return max_sum;
+        }
+    }
+}
